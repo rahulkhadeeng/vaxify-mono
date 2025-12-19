15db12dc744936fe8f7dbc64ebb57d2cb07dc2ae
Abhish7k <abhishekbhosale.dev@gmail.com>
2025-12-19 21:50:09 +0530

    style: next top loader
    added the next js toploader lib component for better ux

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
+import NextTopLoader from "nextjs-toploader";
 
 function App() {
   return (
     <div>
+      <NextTopLoader color="#4f46e5" showSpinner={false} />
+
       <Navbar />
 
       {/* for router */}
